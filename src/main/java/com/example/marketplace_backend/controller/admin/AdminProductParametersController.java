476ab0dc8cf1360c5fbe6fcd6ac3eaf7e6e4740b
JekiChan08 <mistergeki07@gmail.com>
2025-08-11 14:15:55 +0600

    получение всех параметров и саб параметров

--- src/main/java/com/example/marketplace_backend/controller/admin/AdminProductParametersController.java
+++ src/main/java/com/example/marketplace_backend/controller/admin/AdminProductParametersController.java
@@ -1,5 +1,8 @@
 package com.example.marketplace_backend.controller.admin;
 
+import com.example.marketplace_backend.DTO.Responses.models.ProductParameterResponse;
+import com.example.marketplace_backend.Model.ProductParameters;
+import com.example.marketplace_backend.Service.Impl.ConverterService;
 import com.example.marketplace_backend.Service.Impl.ProductParametersServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -18,6 +20,7 @@ import java.util.UUID;
 @RequestMapping("/api/admin/product-parameters")
 public class AdminProductParametersController {
     private final ProductParametersServiceImpl  productParametersService;
+    private final ConverterService converterService;
 
 
     @GetMapping("/stats/product/{productId}")
@@ -28,6 +31,26 @@ public class AdminProductParametersController {
         Map<String, Long> stats = new HashMap<>();
         stats.put("parametersCount", productParametersService.countByProductId(productId));
         return ResponseEntity.ok(stats);
+    }
+
+    @GetMapping("/template")
+    public ResponseEntity<List<ProductParameterResponse>> getProductParametersTemplate() {
+        List<ProductParameters> parameters = productParametersService.findAll();
+
+        // Убираем дубликаты по name и сортируем по name
+        List<ProductParameterResponse> response = parameters.stream()
+                .sorted(Comparator.comparing(ProductParameters::getName))
+                .collect(Collectors.toMap(
+                        ProductParameters::getName,
+                        converterService::convertToProductParameterResponse,
+                        (existing, replacement) -> existing, // если дубликат — берём первый
+                        LinkedHashMap::new
+                ))
+                .values()
+                .stream()
+                .toList();
+
+        return ResponseEntity.ok(response);
     }
 
 
