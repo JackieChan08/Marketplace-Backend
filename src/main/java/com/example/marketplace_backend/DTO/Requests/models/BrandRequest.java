b63577fdd3a6299e793b6ad34ac09eb970d98715
aiteginbaratov <redmifly@gmail.com>
2025-09-25 17:10:02 +0600

    Задачи: 1.product name and availability в поле dto response cart, order, favorite 2.При clear cartItems не очищается get cartItems 3.Пофиксил editBrand

--- src/main/java/com/example/marketplace_backend/DTO/Requests/models/BrandRequest.java
+++ src/main/java/com/example/marketplace_backend/DTO/Requests/models/BrandRequest.java
@@ -1,13 +1,15 @@
 package com.example.marketplace_backend.DTO.Requests.models;
 
-import com.example.marketplace_backend.Model.FileEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
-
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BrandRequest {
     private String name;
     private MultipartFile image;
